fb6ca0d38be599f59a6f0b82f87040fd662c6622
Carlos10398 <75910244+Carlos10398@users.noreply.github.com>
2023-07-10 18:25:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -68,3 +68,20 @@ def get_director(nombre_director):
     print('El número de películas que ha dirigido es:', directors_count)
     print('Sus películas fueron:', director_titles)
     print('Sus ganancias totales son:', director_return)
+@app.get("/recomendacion/{titulo}")
+def recomendacion(titulo: str):
+    idx = muestra_aleatoria[muestra_aleatoria['title'] == titulo].index[0] # Buscamos el indice titulo en nuestro datasets
+
+    sim_cosine = list(enumerate(cosine_similarity[idx])) # Accedemos a la fila 'idx' de la matriz 'simitulud coseno' enumeramos filas, creamos lista de 
+                                                             #tuplas, donde cada tupla contiene el indice y similitud coseno de la pelicula
+    
+
+    sim_scores = sorted(sim_cosine, key=lambda x: x[1], reverse=True) # Ordenamos la lista de tuplas en funcion de la similitud coseno de manera descendente,
+                                                                         #guardamos resultados en variable sim_scores
+    
+
+    similar_indices = [i for i, _ in sim_scores[1:6]] # Creamos lista de las 5 mejores primeras peliculas
+
+    similar_movies = muestra_aleatoria['title'].iloc[similar_indices].values.tolist() # Seleccionamos los titulos segun los indices y los pasamos a una lista
+    
+    return similar_movies #Retornamos la lista
